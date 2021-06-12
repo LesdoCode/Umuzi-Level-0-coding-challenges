ddd79a796bd014e55abb826f0355abe6d447bdd0
LesdoCode <82846153+LesdoCode@users.noreply.github.com>
2021-06-12 22:50:46 +0200

    complete task-05.js

---
+++ task-05.js
@@ -0,0 +1,10 @@
+function getTriangleArea(side1, side2, side3) {
+    let semiPerimeter = (side1 + side2 + side3) / 2;
+
+    return Math.sqrt(
+        semiPerimeter * (semiPerimeter - side1) *
+        (semiPerimeter - side2) *
+        (semiPerimeter - side3)
+    );
+
+}
