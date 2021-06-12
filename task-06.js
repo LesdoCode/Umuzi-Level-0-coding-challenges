000da77340349b178bc854e2415d2a13aa8d7b01
LesdoCode <82846153+LesdoCode@users.noreply.github.com>
2021-06-12 23:13:05 +0200

    complete task-06.js

---
+++ task-06.js
@@ -0,0 +1,11 @@
+function findLargest() {
+    let largest = arguments['0'];
+
+    for (let key in arguments) {
+        if (arguments[key] > largest)
+            largest = arguments[key]
+    }
+    return largest;
+}
+
+findLargest()
