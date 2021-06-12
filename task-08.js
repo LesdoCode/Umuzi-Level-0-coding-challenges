de6e895215e2507f33845cc6faa0e3e0c9d4eb2a
LesdoCode <82846153+LesdoCode@users.noreply.github.com>
2021-06-12 23:56:15 +0200

    complete task-08.js

---
+++ task-08.js
@@ -0,0 +1,28 @@
+function decipherTime(number) {
+    let minutes = number % 60;
+    let hours = number / 60;
+    let time = "\""; //I've only put the quotes first because the question literal looks that way in both cases. Just to be sure.
+
+    if (hours >= 1)
+        if (hours < 2)
+            time += "1 hour";
+        else
+            time += Math.floor(hours) + " hours";
+
+    if (hours >= 1 && minutes > 0)
+        time += ", ";
+    else if (minutes <= 0) {
+        time += ".";
+        return time;
+    }
+
+    if (minutes > 0)
+        if (minutes < 2)
+            time += "1 minute.";
+        else
+            time += minutes + " minutes.";
+
+    return time
+}
+
+console.log(decipherTime(550));
