256e596eb49a57f5480f669944aa8bb14f82ae81
LesdoCode <82846153+LesdoCode@users.noreply.github.com>
2021-07-28 10:48:44 +0200

    Give variables more meaningful names

--- task-01.js
+++ task-01.js
@@ -1,11 +1,11 @@
-let x = 0,
-	y = 1;
+let firstNumber = 0
+let secondNumber = 1;
 
-console.log(x);
-console.log(y);
+console.log(firstNumber);
+console.log(secondNumber);
 
-x += 3;
-y += x;
+firstNumber += 3;
+secondNumber += x;
 
-console.log(x);
-console.log(y);
+console.log(firstNumber);
+console.log(secondNumber);
